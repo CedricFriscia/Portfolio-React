6858a92e975af72146d7edf2904d091b8ab8a65a
CedricFriscia <cedricfriscia@gmail.com>
2023-05-09 12:05:32 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container'>
+        <header className='header'>
+          <div className="header__left-screen">
+
+          </div>
+          <div className="header__right-screen">
+            
+          </div>
+        </header>
+        <main>
+
+        </main>
+    </div>
+  )
+}
+
+export default App
